52d426f0260be17ca40cb8380a3d2d5af17e3380
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2021-12-24 10:31:34 +0530

    Added Application

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Quote-Generator
+This quote generator generates quotes every day so that you start your dy with a positive attitude.
+
+## About the Project
+This application is written in Python 3.7 by using the following modules
+1. Tkinter
+2. Random
+3. Requests
+
+This application will work in Windows, Mac and Linux.
+
+# Images
+![Screenshot](https://user-images.githubusercontent.com/61897464/147318356-42a16f6d-79ab-42f3-bc6b-d885b74f266c.png)
+
+![Screenshot](https://user-images.githubusercontent.com/61897464/147318462-3631604c-1f0f-49fa-bbc7-998d9e8397e1.png)
+
---
+++ quote_generator.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import requests
+import random
+from threading import Thread
+class QuoteGenerator:
+
+    def __init__(self,master):
+
+        self.master=master
+        self.master.title("Quote Generator")
+        self.master.geometry("1000x250")
+
+        self.quote_label=Label(self.master,wraplength=800,font=("Courier New",14))
+        self.quote_label.configure(text="")
+        self.quote_label.pack(ipadx=20,ipady=20,padx=20,pady=20)
+
+        self.generate_quote_button=Button(self.master,text='Generate!',command=self.generate,width=30,font=("Courier New",14),bg="#0c20f7",activebackground="#2839f7",fg="white",activeforeground="white")
+        self.generate_quote_button.pack()
+
+
+
+
+        self.master.mainloop()
+
+
+
+    def generate(self):
+        self.quote_label.configure(text="")
+        quote_json=requests.get("http://api.quotable.io/random").json()
+        quote="{}\n\nBy: {}".format(quote_json['content'],quote_json['author'])
+        print(quote)
+        self.quote_label.configure(text=quote)
+
+    
+if __name__=="__main__":
+    root=Tk()
+    app=QuoteGenerator(root)
